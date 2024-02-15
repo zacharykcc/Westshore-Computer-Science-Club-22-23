9b1021d71bd2bab955d10eaa608816352a765a68
zacharykcc <zacharykcc.wales@gmail.com>
2024-02-14 23:52:27 -0500

    dasdgfadsf

--- MakeABudget/code.py
+++ MakeABudget/code.py
@@ -1,17 +1,47 @@
-# Games, Housing, Dating, Hobbies, School, Groceries, Automotive, Streaming, Sports, Concerts, HomeImprovement, 
 with open("chal.txt", "r") as file:
     content = file.readlines()
+
 posistion = 1
 columns = 0
 rows = 0
+tempList = []
+matrixPos = 0
+'''
+print(content)
+print(posistion)
+print((str(content[posistion])[:1]))
+'''
+
+
 
 for count in range(int(content[0])):
+    
     matrix = [[],
               []]
     for count in range( (int(str(content[posistion])[:1]))):
         posistion += 1
-        matrix.append( ((content[posistion]).split())[0] )
+        tempList.append( ((content[posistion]).split())[0] )
         columns += 1
-    
-    for count in range(int(str(content[posistion])[1:2]) ):
-        print("filler")
+    matrix[0].append(tempList) 
+    tempList =[]
+
+    for count in range( (int(str( content[posistion].split()[1] ))) - 1 ): 
+
+        
+        print(posistion)
+        print(content[posistion])
+        tempList.append( ((content[posistion]).split())[1] ) # shit break line
+        posistion += 1
+        rows += 1
+        
+    matrix[1].append(tempList) 
+    tempList =[]
+
+    for count in range(int(str(content[posistion-columns])[1:2]) ):
+        matrixPos = 0
+        if str( ((content[posistion]).split())[0] ) == matrix[0][matrixPos]:
+            if ( matrix[1][matrixPos] + int( ((content[posistion]).split())[1] ) ) >= 0:
+                matrix[1][matrixPos] = matrix[1][matrixPos] + int(  str(content[posistion].split()[1])  + str(content[posistion].split()[2]) )
+                print("YES")
+            else:
+                print("NO")
